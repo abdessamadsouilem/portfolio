c44955aebacbacc4d61be456909113a1ae2820ca
abdessamad <abdessamadsouilem1@gmail.com>
2021-04-21 14:09:13 +0000

    new-projects

---
+++ portfolio/src/components/Skills.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="skils">
+      <br>
+      <h1>SKILLS</h1>
+      <p>HERE IS MY ARMAMENT</p>
+      <hr />
+      <br>
+      <img class="im" src="../assets/html.png" alt="">
+      <img class="im" src="../assets/css.png" alt="">
+      <img class="im" src="../assets/linux.png" alt="">
+      <img class="im" src="../assets/java.png" alt="">
+      <img class="im" src="../assets/C.png" alt="">
+      <img class="im" src="../assets/cplusplus.png" alt="">
+      <br>
+      <br>
+      <img class="im" src="../assets/Csharp.png" alt="">
+      <img class="im" src="../assets/php.png" alt="">
+      <img class="im" src="../assets/vuejs.png" alt="">
+      <img class="im" src="../assets/Octocat.png" alt="">
+      <img class="im" src="../assets/go.png" alt="">
+      <img class="im" src="../assets/laravel.png" alt="">
+      <br>
+      <br>
+      <img class="im" src="../assets/unity.png" alt="">
+      <img class="im" src="../assets/ovh.png" alt="">
+      <img class="im" src="../assets/android.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+hr{
+  width: 10%;
+  border:1px solid black;;
+}
+.im{
+    border-radius: inherit;
+    margin: 20px;
+}
+</style>
