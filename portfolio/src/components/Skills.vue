15ae318e678af8b2297554224934e909321288d0
abdessamad <abdessamadsouilem1@gmail.com>
2021-04-26 03:29:23 +0000

    commit

--- portfolio/src/components/Skills.vue
+++ portfolio/src/components/Skills.vue
@@ -1,29 +1,82 @@
 <template>
   <div class="skils">
       <br>
-      <h1>SKILLS</h1>
-      <p>HERE IS MY ARMAMENT</p>
-      <hr />
+      <div class="id" data-aos="fade-down">
+      <h1 >SKILLS</h1>
+      <p >HERE IS MY ARMAMENT</p>
+      </div>
+      
       <br>
-      <img class="im" src="../assets/html.png" alt="">
-      <img class="im" src="../assets/css.png" alt="">
+      <div class="skil d-flex flex-wrap justify-content-center">
+        <div class="all d-flex flex-wrap justify-content-center">
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/html.png" alt="">
+      <p>Html</p>
+    </div>
+    <div data-aos="zoom-in">
+      <img  class="im" src="../assets/css.png" alt="">
+      <p>Css</p>
+      </div>
+      <div data-aos="zoom-in">
       <img class="im" src="../assets/linux.png" alt="">
-      <img class="im" src="../assets/java.png" alt="">
-      <img class="im" src="../assets/C.png" alt="">
-      <img class="im" src="../assets/cplusplus.png" alt="">
+      <p>Linux</p>
+      </div>
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/java.png" alt="">
+      <p>Java</p>
+      </div>
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/C.png" alt="">
+      <p>C</p>
+      </div>
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/cplusplus.png" alt="">
+      <p>C+</p>
+      </div>
       <br>
       <br>
-      <img class="im" src="../assets/Csharp.png" alt="">
-      <img class="im" src="../assets/php.png" alt="">
-      <img class="im" src="../assets/vuejs.png" alt="">
-      <img class="im" src="../assets/Octocat.png" alt="">
-      <img class="im" src="../assets/go.png" alt="">
-      <img class="im" src="../assets/laravel.png" alt="">
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/Csharp.png" alt="">
+      <p>C#</p>
+      </div>
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/php.png" alt="">
+      <p>php</p>
+      </div>
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/vuejs.png" alt="">
+      <p>Vue js</p>
+      </div>
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/Octocat.png" alt="">
+      <p>Octocat</p>
+      </div>
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/go.png" alt="">
+      <p>Go</p>
+      </div>
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/laravel.png" alt="">
+      <p>Laravel</p>
+      </div>
       <br>
       <br>
-      <img class="im" src="../assets/unity.png" alt="">
-      <img class="im" src="../assets/ovh.png" alt="">
-      <img class="im" src="../assets/android.png" alt="">
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/unity.png" alt="">
+      <p>Unity</p>
+      </div>
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/ovh.png" alt="">
+      <p>ovh</p>
+      </div>
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/android.png" alt="">
+      <p>android</p>
+      </div>
+      </div>
+      </div>
+      <br>
+      <br>
   </div>
 </template>
 
@@ -34,12 +87,21 @@ export default {
 </script>
 
 <style scoped>
-hr{
-  width: 10%;
-  border:1px solid black;;
+.id:after {
+      content: "";
+      width: 170px;
+      display: block;
+      margin: 20px auto;
+      border-bottom: 2px solid black;
 }
 .im{
+    
+    max-width: 120px;
+    max-height: 120px;
     border-radius: inherit;
-    margin: 20px;
+    margin: 50px;
+}
+.all{
+  width: 90%;
 }
 </style>
--- portfolio/src/components/project.vue
+++ portfolio/src/components/project.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
-
+  <div class="all">
+    <br>
+   <div class="id" data-aos="fade-down">
+      <h1>MY PROJECTS</h1>
+      <p>THIS IS MY STORY</p>
+   </div>
+   
+   <br>
+   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto dignissimos rem fuga fugit enim sequi architecto, quis corrupti iure dolor temporibus eos eum quas atque est? Magnam excepturi molestias maiores.</p>
+   <br>
+  
   </div>
 </template>
 
-<script>
-export default {
 
-}
+<script>
+
 </script>
 
 <style scoped>
+
+.all{
+ background-color: #424242;
+    color: white;
+    
+}
+h1{
+  font-size: 50px;
+}
+.all hr{
+  width: 12%;
+  border:1px solid white;
+}
+.id:after {
+      content: "";
+      width: 170px;
+      display: block;
+      margin: 20px auto;
+      border-bottom: 2px solid white;
+}
 
 </style>
