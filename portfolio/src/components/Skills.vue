9440b01d9261d402f73a7c7261e3f46d57f092d1
abdessamad <abdessamadsouilem1@gmail.com>
2021-04-26 22:16:47 +0000

    edit

--- portfolio/src/components/Skills.vue
+++ portfolio/src/components/Skills.vue
@@ -21,24 +21,10 @@
       <img class="im" src="../assets/linux.png" alt="">
       <p>Linux</p>
       </div>
-      <div data-aos="zoom-in">
-      <img  class="im" src="../assets/java.png" alt="">
-      <p>Java</p>
-      </div>
-      <div data-aos="zoom-in">
-      <img  class="im" src="../assets/C.png" alt="">
-      <p>C</p>
-      </div>
-      <div data-aos="zoom-in">
-      <img  class="im" src="../assets/cplusplus.png" alt="">
-      <p>C+</p>
-      </div>
+      
       <br>
       <br>
-      <div data-aos="zoom-in">
-      <img  class="im" src="../assets/Csharp.png" alt="">
-      <p>C#</p>
-      </div>
+      
       <div data-aos="zoom-in">
       <img  class="im" src="../assets/php.png" alt="">
       <p>php</p>
@@ -47,31 +33,27 @@
       <img  class="im" src="../assets/vuejs.png" alt="">
       <p>Vue js</p>
       </div>
-      <div data-aos="zoom-in">
-      <img  class="im" src="../assets/Octocat.png" alt="">
-      <p>Octocat</p>
-      </div>
-      <div data-aos="zoom-in">
-      <img  class="im" src="../assets/go.png" alt="">
-      <p>Go</p>
-      </div>
-      <div data-aos="zoom-in">
-      <img  class="im" src="../assets/laravel.png" alt="">
-      <p>Laravel</p>
-      </div>
+      
+      
+      
       <br>
       <br>
+      
       <div data-aos="zoom-in">
-      <img  class="im" src="../assets/unity.png" alt="">
-      <p>Unity</p>
+      <img  class="im" src="../assets/icons8-javascript-480.png" alt="">
+      <p>Javascript</p>
       </div>
       <div data-aos="zoom-in">
-      <img  class="im" src="../assets/ovh.png" alt="">
-      <p>ovh</p>
+      <img  class="im" src="../assets/icons8-bootstrap-480.png" alt="">
+      <p>bootstrap</p>
       </div>
       <div data-aos="zoom-in">
-      <img  class="im" src="../assets/android.png" alt="">
-      <p>android</p>
+      <img  class="im" src="../assets/icons8-adobe-xd-480.png" alt="">
+      <p>adobe-xd</p>
+      </div>
+      <div data-aos="zoom-in">
+      <img  class="im" src="../assets/icons8-github-240.png" alt="">
+      <p>github</p>
       </div>
       </div>
       </div>
